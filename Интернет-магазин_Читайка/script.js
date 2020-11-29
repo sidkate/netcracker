3cf7d25dbf8a473129d1785f3d188f4d93ec7910
Sidorenkova Kate <sidorenkova.katia@yandex.ru>
2020-11-29 21:08:59 +0400

    creating ShoppingCart class

--- Интернет-магазин_Читайка/script.js
+++ Интернет-магазин_Читайка/script.js
@@ -1,10 +1,8 @@
-let counter = 0;
-let products = new Map();
-
 let basketElement;
 let loader;
 let popup;
 let popupList;
+let shoppingCart;
 
 window.onload = () => {
     // initializing global variables after page was loaded
@@ -12,102 +10,113 @@ window.onload = () => {
     loader = document.getElementById('loader');
     popup = document.getElementById('popup');
     popupList = document.getElementById('popupList');
+
+    shoppingCart = new ShoppingCart();
 }
 
-function addItemToBasket(title, price, id) {
+class ShoppingCart {
 
-    counter++;
+    constructor() {
+        this.counter = 0;
+        this.products = new Map();
+    }
 
-    basketElement.innerText = counter;
+    addItemToBasket(title, price, id) {
 
-    let product = products.get(id);
+        this.counter++;
 
-    if (product) {
-        product.count++;
-    } else {
-        products.set(id, {
-            title: title,
-            price: price,
-            id: id,
-            count: 1
+        basketElement.innerText = this.counter;
+
+        let product = this.products.get(id);
+
+        if (product) {
+            product.count++;
+        } else {
+            this.products.set(id, {
+                title: title,
+                price: price,
+                id: id,
+                count: 1
+            });
+        }
+
+        this.updatePopupData();
+
+        this.showLoader(() => {
+            this.hideLoader();
+            this.openPopup();
         });
     }
 
-    updatePopupData();
+    updatePopupData() {
 
-    showLoader(() => {
-        hideLoader();
-        openPopup();
-    });
-}
+        popupList.innerHTML = '';
 
-function updatePopupData() {
+        for (let [id, product] of this.products) {
 
-    popupList.innerHTML = '';
+            //let product = products[id];
+            // destructuring assignment example
+            // let [title, count, price] = [product.title, product.count, product.price];
+            // let cost = count * price;
+            let basketItemTemplate = `
+                <td>${product.title}</td>
+                <td>${product.count} шт. </td>
+                <td>${product.count * product.price} руб. </td>
+                <td class="delete-item-basket" onclick="shoppingCart.deleteProduct(${id})"> + </td>
+            `;
 
-    for (let [id, product] of products) {
+            let basketItem = document.createElement('tr');
+            /*let title = document.createElement('td');
+            let count = document.createElement('td');
+            let price = document.createElement('td');
+            let itemDelete = document.createElement('td');
+    
+            title.innerText = product.title;
+            count.innerText = product.count + ' шт. ';
+            price.innerText = product.count * product.price + ' руб. ';
+            itemDelete.innerText = " + ";*/
+            basketItem.innerHTML = basketItemTemplate;
+            //let itemDelete = basketItem.getElementsByClassName
+            // itemDelete.onclick = () => {
+            //     deleteProduct(id);
+            // };
 
-        //let product = products[id];
-        // destructuring assignment example
-        // let [title, count, price] = [product.title, product.count, product.price];
-        // let cost = count * price;
-        let basketItemTemplate = `
-            <td>${product.title}</td>
-            <td>${product.count} шт. </td>
-            <td>${product.count * product.price} руб. </td>
-            <td class="delete-item-basket" onclick="deleteProduct(${id})"> + </td>
-        `;
+            //itemDelete.classList.add('delete-item-basket');
 
-        let basketItem = document.createElement('tr');
-        /*let title = document.createElement('td');
-        let count = document.createElement('td');
-        let price = document.createElement('td');
-        let itemDelete = document.createElement('td');
-
-        title.innerText = product.title;
-        count.innerText = product.count + ' шт. ';
-        price.innerText = product.count * product.price + ' руб. ';
-        itemDelete.innerText = " + ";*/
-        basketItem.innerHTML = basketItemTemplate;
-        //let itemDelete = basketItem.getElementsByClassName
-        // itemDelete.onclick = () => {
-        //     deleteProduct(id);
-        // };
-
-        //itemDelete.classList.add('delete-item-basket');
-
-        popupList.appendChild(basketItem);
-        /*basketItem.appendChild(title);
-        basketItem.appendChild(count);
-        basketItem.appendChild(price);
-        basketItem.appendChild(itemDelete);*/
+            popupList.appendChild(basketItem);
+            /*basketItem.appendChild(title);
+            basketItem.appendChild(count);
+            basketItem.appendChild(price);
+            basketItem.appendChild(itemDelete);*/
+        }
     }
-}
 
-function deleteProduct(id) {
-    let product = products.get(id);
-    if (product) {
-        counter -= product.count;
-        basketElement.innerText = counter;
-        products.delete(id);
-        updatePopupData();
+    deleteProduct(id) {
+        let product = this.products.get(id);
+        if (product) {
+            this.counter -= product.count;
+            basketElement.innerText = this.counter;
+            this.products.delete(id);
+            this.updatePopupData();
 
+        }
     }
-}
 
-function openPopup() {
-    popup.style.visibility = 'visible';
-}
+    openPopup() {
+        popup.style.visibility = 'visible';
+    }
 
-function closePopup() {
-    popup.style.visibility = 'hidden';
-}
+    closePopup() {
+        popup.style.visibility = 'hidden';
+    }
 
-function showLoader(callback) {
-    loader.style.visibility = 'visible';
-    setTimeout(callback, 2000);
-}
+    showLoader(callback) {
+        loader.style.visibility = 'visible';
+        setTimeout(callback, 2000);
+    }
 
-function hideLoader() {
-    loader.style.visibility = 'hidden';
+    hideLoader() {
+        loader.style.visibility = 'hidden';
+    }
+
 }
