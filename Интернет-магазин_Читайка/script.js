7bb3b0dec18b60114ae839619c8426ab5448789b
Sidorenkova Kate <sidorenkova.katia@yandex.ru>
2020-12-05 19:52:45 +0400

    added Couter and other changes

--- Интернет-магазин_Читайка/script.js
+++ Интернет-магазин_Читайка/script.js
@@ -1,42 +1,40 @@
 let basketElement;
 let loader;
 let popup;
-let popupList;
 let shoppingCart;
 
 window.onload = () => {
     // initializing global variables after page was loaded
     basketElement = document.getElementById('basket');
     loader = document.getElementById('loader');
-    popup = document.getElementById('popup');
-    popupList = document.getElementById('popupList');
+    //popup = document.getElementById('popup');
+    //popupList = document.getElementById('popupList');
 
-    shoppingCart = new ShoppingCart();
+    let container = document.getElementById('container');
+    shoppingCart = new ShoppingCart(container);
 }
 
 class ShoppingCart {
 
-    constructor() {
+    constructor(wrapper) {
         this.counter = 0;
         this.products = new Map();
+        this.popupWrapper = wrapper;
     }
 
     addItemToBasket(title, price, id) {
-
-        this.counter++;
-
-        basketElement.innerText = this.counter;
+        if (popup)
+            return;
 
         let product = this.products.get(id);
 
-        if (product) {
-            product.addQuantity();
-        } else {
+        if (!product) {
             product = new Product(id, title, price);
             this.products.set(id, product);
         }
+        product.plus();
 
-        this.updatePopupData();
+        //this.updatePopupData();
 
         this.showLoader(() => {
             this.hideLoader();
@@ -46,30 +44,60 @@ class ShoppingCart {
 
     updatePopupData() {
 
-        popupList.innerHTML = '';
+        this.popupList.innerHTML = '';
 
         for (let [id, product] of this.products) {
-            product.appendToPopupList();
+            product.appendToPopupList(this.popupList);
         }
+
+        if (this.counter == 0)
+            this.closePopup();
     }
 
     deleteProduct(id) {
         let product = this.products.get(id);
         if (product) {
             this.counter -= product.count;
-            basketElement.innerText = this.counter;
+            this.updateBasketCounter();
             this.products.delete(id);
             this.updatePopupData();
-
         }
     }
 
     openPopup() {
-        popup.style.visibility = 'visible';
+        if (popup)
+            return;
+
+        let popupTemplate = `
+            <span class="close-popup" onclick="shoppingCart.closePopup()">+</span>
+            <p class="heading-popup">Товары в Вашей корзине:</p>
+            <table class="heading-list-popup" width="500">
+            <tr>
+                <td>наименование</td>
+                <td>количество</td>
+                <td>стоимость</td>
+            </tr>
+            </table>
+            <table class="popap-list" id="popupList" width="500">
+            </table>
+        `;
+        let popupElement = document.createElement('div');
+        popupElement.id = 'popup';
+        popupElement.classList.add('popup');
+        popupElement.innerHTML = popupTemplate;
+        this.popupWrapper.appendChild(popupElement);
+        this.popupList = document.getElementById('popupList');
+
+        popup = popupElement;
+
+        this.updatePopupData();
     }
 
     closePopup() {
-        popup.style.visibility = 'hidden';
+        if (popup) {
+            this.popupWrapper.removeChild(popup);
+            popup = undefined;
+        }
     }
 
     showLoader(callback) {
@@ -81,6 +109,22 @@ class ShoppingCart {
         loader.style.visibility = 'hidden';
     }
 
+    plus() {
+        this.counter++;
+        this.updateBasketCounter();
+    }
+
+    minus() {
+        if (this.counter > 0) {
+            this.counter--;
+            this.updateBasketCounter();
+        }
+    }
+
+    updateBasketCounter() {
+        basketElement.innerText = this.counter;
+    }
+
 }
 
 class Product {
@@ -89,19 +133,30 @@ class Product {
         this.id = id;
         this.title = title;
         this.price = price;
-        this.count = 1;
+        this.count = 0;
     }
 
-    addQuantity() {
+    plus() {
         this.count++;
+        shoppingCart.plus();
     }
 
-    appendToPopupList() {
+    minus() {
+        if (this.count > 1) {
+            this.count--;
+            shoppingCart.minus();
+        } else {
+            shoppingCart.deleteProduct(this.id);
+        }
+    }
+
+    appendToPopupList(wrapper) {
         // destructuring assignment example
         // let [title, count, price] = [this.title, this.count, this.price];
         // let cost = count * price;
         let basketItemTemplate = `
             <td>${this.title}</td>
+            <td class="counter"></td>
             <td>${this.count} шт. </td>
             <td>${this.count * this.price} руб. </td>
             <td class="delete-item-basket" onclick="shoppingCart.deleteProduct(${this.id})"> + </td>
@@ -125,10 +180,51 @@ class Product {
 
         //itemDelete.classList.add('delete-item-basket');
 
-        popupList.appendChild(basketItem);
+        wrapper.appendChild(basketItem);
+        let counterWrapper = basketItem.getElementsByClassName('counter')[0];
+        new Counter(counterWrapper, this);
         /*basketItem.appendChild(title);
         basketItem.appendChild(count);
         basketItem.appendChild(price);
         basketItem.appendChild(itemDelete);*/
     }
+}
+
+class Counter {
+
+    constructor(wrapper, product) {
+        this.product = product;
+        this.wrapper = wrapper;
+        this.createElement();
+        this.updateCounterValue();
+    }
+
+    createElement() {
+        let counterTemplate = `
+            <div class="minus">-</div>
+            <div class="counter-value"></div>
+            <div class="plus">+</div>
+        `;
+        this.wrapper.innerHTML = counterTemplate;
+        let minusElement = this.wrapper.getElementsByClassName('minus')[0];
+        let plusElement = this.wrapper.getElementsByClassName('plus')[0];
+        minusElement.onclick = () => { this.minus() };
+        plusElement.onclick = () => { this.plus() };
+    }
+
+    updateCounterValue() {
+        let counterValueElement = this.wrapper.getElementsByClassName('counter-value')[0];
+        counterValueElement.innerText = this.product.count;
+    }
+
+    plus() {
+        this.product.plus();
+        this.updateCounterValue();
+    }
+
+    minus() {
+        this.product.minus();
+        this.updateCounterValue();
+    }
+
 }
